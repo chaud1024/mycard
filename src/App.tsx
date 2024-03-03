58486ee29ffafb770f6e10d8a3af77e31c03cfcf
chaud1024 <chaud1024@gmail.com>
2024-03-03 19:30:36 +0900

    생성: NavBar 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import CardPage from '@pages/Card'
 import Home from '@pages/Home'
+import SigninPage from '@pages/Signin'
+import SignupPage from '@pages/Signup'
 import Test from '@pages/Test'
 import ScrollToTop from '@shared/ScrollToTop'
-import CardPage from './pages/Card'
+import NavBar from './components/shared/NavBar'
 
 const App = () => {
   return (
     <BrowserRouter>
       <ScrollToTop />
+      <NavBar />
       <Routes>
         <Route path="/" Component={Home} />
+        <Route path="/signin" Component={SigninPage} />
+        <Route path="/signup" Component={SignupPage} />
         <Route path="/card/:id" Component={CardPage} />
         <Route path="/test" Component={Test} />
       </Routes>
---
+++ src/components/shared/NavBar.tsx
@@ -0,0 +1,32 @@
+import { colors } from '@/styles/colorPalette'
+import { css } from '@emotion/react'
+import Button from '@shared/Button'
+import Flex from '@shared/Flex'
+import { Link, useLocation } from 'react-router-dom'
+
+const NavBar = () => {
+  const location = useLocation()
+  const showSignButton =
+    ['/signup', 'signin'].includes(location.pathname) === false
+  return (
+    <Flex align="center" justify="space-between" css={navbarContainerStyles}>
+      <Link to="/">홈</Link>
+      {showSignButton ? (
+        <Link to="/signup">
+          <Button>회원가입/로그인</Button>
+        </Link>
+      ) : null}
+    </Flex>
+  )
+}
+
+const navbarContainerStyles = css`
+  padding: 10px 24px;
+  position: sticky;
+  top: 0;
+  background-color: ${colors.white};
+  z-index: 10;
+  border-bottom: 1px solid ${colors.gray};
+`
+
+export default NavBar
