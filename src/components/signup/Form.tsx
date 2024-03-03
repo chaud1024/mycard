bbd6e132e9ba141ebe342d7fc517983d3f213f89
chaud1024 <chaud1024@gmail.com>
2024-03-03 20:35:40 +0900

    유효성 체크와 에러메시지 처리

--- src/components/signup/Form.tsx
+++ src/components/signup/Form.tsx
@@ -4,19 +4,33 @@ import FixedBottomButton from '@shared/FixedBottomButton'
 import Flex from '@shared/Flex'
 import Spacing from '@shared/Spacing'
 import TextField from '@shared/TextField'
-import { ChangeEvent, useCallback, useState } from 'react'
+import { ChangeEvent, useCallback, useMemo, useState } from 'react'
+import validator from 'validator'
 
 const Form = () => {
-  const [formValue, setFormValue] = useState<FormValues>({
+  const [formValues, setFormValues] = useState<FormValues>({
     email: '',
     password: '',
     rePassword: '',
     name: '',
   })
 
+  const [dirty, setDirty] = useState<Partial<FormValues>>({})
+
   const handlelFormValues = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setFormValue((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+    setFormValues((prev) => ({ ...prev, [e.target.name]: e.target.value }))
   }, [])
+
+  const handleBlur = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setDirty((prevDirty) => ({
+      ...prevDirty,
+      [e.target.name]: 'true',
+    }))
+  }, [])
+
+  const errors = useMemo(() => validate(formValues), [formValues])
+
+  const 제출가능한상태 = Object.keys(errors).length === 0
 
   return (
     <Flex direction="column" css={formContainerStyles}>
@@ -24,35 +38,51 @@ const Form = () => {
         label="이메일"
         name="email"
         placeholder="hong@naver.com"
-        value={formValue.email}
+        value={formValues.email}
         onChange={handlelFormValues}
+        hasError={Boolean(dirty.email) && Boolean(errors.email)}
+        helpMessage={Boolean(dirty.email) ? errors.email : ''}
+        onBlur={handleBlur}
       />
       <Spacing size={16} />
       <TextField
         label="패스워드"
         name="password"
         type="password"
-        value={formValue.password}
+        value={formValues.password}
         onChange={handlelFormValues}
+        hasError={Boolean(dirty.password) && Boolean(errors.password)}
+        helpMessage={Boolean(dirty.password) ? errors.password : ''}
+        onBlur={handleBlur}
       />
       <Spacing size={16} />
       <TextField
         label="패스워드 재확인"
         name="rePassword"
         type="password"
-        value={formValue.rePassword}
+        value={formValues.rePassword}
         onChange={handlelFormValues}
+        hasError={Boolean(dirty.rePassword) && Boolean(errors.rePassword)}
+        helpMessage={Boolean(dirty.rePassword) ? errors.rePassword : ''}
+        onBlur={handleBlur}
       />
       <Spacing size={16} />
       <TextField
         label="이름"
         name="name"
         placeholder="홍길동"
-        value={formValue.name}
+        value={formValues.name}
         onChange={handlelFormValues}
+        hasError={Boolean(dirty.name) && Boolean(errors.name)}
+        helpMessage={Boolean(dirty.name) ? errors.name : ''}
+        onBlur={handleBlur}
       />
 
-      <FixedBottomButton label="회원가입" disabled={true} onClick={() => {}} />
+      <FixedBottomButton
+        label="회원가입"
+        disabled={제출가능한상태 === false}
+        onClick={() => {}}
+      />
     </Flex>
   )
 }
@@ -60,5 +90,30 @@ const Form = () => {
 const formContainerStyles = css`
   padding: 24px;
 `
+function validate(formValues: FormValues) {
+  let errors: Partial<FormValues> = {}
+
+  if (validator.isEmail(formValues.email) === false) {
+    errors.email = '이메일 형식을 확인해주세요.'
+  }
+
+  if (formValues.password.length < 8) {
+    errors.password = '비밀번호는 8글자 이상 입력해주세요.'
+  }
+
+  if (formValues.rePassword.length < 8) {
+    errors.rePassword = '비밀번호는 8글자 이상 입력해주세요.'
+  } else if (
+    validator.equals(formValues.password, formValues.rePassword) === false
+  ) {
+    errors.rePassword = '비밀번호를 확인해주세요.'
+  }
+
+  if (formValues.name.length < 2) {
+    errors.name = '이름은 2글자 이상 입력해주세요.'
+  }
+
+  return errors
+}
 
 export default Form
