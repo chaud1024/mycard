4cfc7b3c3603248b0621b1bf6c96d3bd5a1be124
chaud1024 <chaud1024@gmail.com>
2024-03-03 14:16:42 +0900

    react query-useInfiniteQuery 를 사용한 페이징 처리

--- src/components/home/CardList.tsx
+++ src/components/home/CardList.tsx
@@ -1,18 +1,34 @@
-import { useQuery } from 'react-query'
 import { getCards } from '@/remote/card'
-
+import { useInfiniteQuery } from 'react-query'
+import { flatten } from 'lodash'
 import ListRow from '@shared/ListRow'
 
 const CardList = () => {
-  const { data } = useQuery(['card'], () => getCards())
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(
+    ['card'],
+    ({ pageParam }) => {
+      return getCards(pageParam)
+    },
+    {
+      getNextPageParam: (snapshot) => {
+        return snapshot.lastVisible
+      },
+    },
+  )
+
+  console.log('data', data)
 
   if (data == null) {
     return null
   }
+
+  const cards = flatten(data?.pages.map(({ items }) => items))
+
   return (
     <div>
+      <button onClick={() => fetchNextPage()}>데이터 불러오기</button>
       <ul>
-        {data.map((card, index) => {
+        {cards.map((card, index) => {
           return (
             <ListRow
               key={card.id}
