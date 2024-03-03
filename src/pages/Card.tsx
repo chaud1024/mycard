e391a996bb7b8b952311bce3f27eb226cebd7f3c
chaud1024 <chaud1024@gmail.com>
2024-03-03 16:50:50 +0900

    카드 혜택 나열

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -1,3 +1,4 @@
+import ListRow from '@/components/shared/ListRow'
 import { getCard } from '@remote/card'
 import Top from '@shared/Top'
 import { useQuery } from 'react-query'
@@ -14,14 +15,53 @@ const CardPage = () => {
     return null
   }
 
-  const { name, corpName, promotion, tags } = data
+  const { name, corpName, promotion, tags, benefit } = data
   const subTitle =
     promotion != null ? removeHtmlTags(promotion.title) : tags.join(', ')
 
   return (
     <div>
       <Top title={`${corpName} ${name}`} subTitle={subTitle} />
+      <ul>
+        {benefit.map((text, index) => (
+          <ListRow
+            key={text}
+            left={<IconCheck />}
+            contents={
+              <ListRow.Texts title={`혜택 ${index + 1}`} subTitle={text} />
+            }
+          />
+        ))}
+      </ul>
     </div>
+  )
+}
+
+const IconCheck = () => {
+  return (
+    <svg
+      fill="none"
+      height="24"
+      width="24"
+      viewBox="0 0 48 48"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect fill="white" fillOpacity="0.01" height="48" width="48" />
+      <path
+        d="M24 44C29.5228 44 34.5228 41.7614 38.1421 38.1421C41.7614 34.5228 44 29.5228 44 24C44 18.4772 41.7614 13.4772 38.1421 9.85786C34.5228 6.23858 29.5228 4 24 4C18.4772 4 13.4772 6.23858 9.85786 9.85786C6.23858 13.4772 4 18.4772 4 24C4 29.5228 6.23858 34.5228 9.85786 38.1421C13.4772 41.7614 18.4772 44 24 44Z"
+        fill="#2F88FF"
+        stroke="black"
+        strokeLinejoin="round"
+        strokeWidth="4"
+      />
+      <path
+        d="M16 24L22 30L34 18"
+        stroke="white"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="4"
+      />
+    </svg>
   )
 }
 
