d6a5f51d263150ed48be8e2160efa91b73c6fa3e
chaud1024 <chaud1024@gmail.com>
2024-03-03 18:06:27 +0900

    pathname을 감지하여 앞의 스크롤의 뒤의 스크롤에 영향을 끼치지 않도록 함

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Home from '@pages/Home'
 import Test from '@pages/Test'
+import ScrollToTop from '@shared/ScrollToTop'
 import CardPage from './pages/Card'
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/card/:id" Component={CardPage} />
