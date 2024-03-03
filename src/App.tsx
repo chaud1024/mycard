26fa05f9cee6b7120f3f62d7db72b848726a5649
chaud1024 <chaud1024@gmail.com>
2024-03-03 16:40:44 +0900

    설정 : 카드 상세화면 router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Home from '@pages/Home'
 import Test from '@pages/Test'
+import CardPage from './pages/Card'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={Home} />
+        <Route path="/card/:id" Component={CardPage} />
         <Route path="/test" Component={Test} />
       </Routes>
     </BrowserRouter>
