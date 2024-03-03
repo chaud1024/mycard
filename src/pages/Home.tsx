2fcc1528e2960f7de91e023e6ba874e8d1630921
chaud1024 <chaud1024@gmail.com>
2024-03-03 13:23:53 +0900

    reactQuery-useQuery를 사용하여 AdBanner 보여주기 w/ Swiper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,14 @@
-import { useEffect } from 'react'
-
+import AdBanners from '@/components/home/AdBanners'
 import Top from '@/components/shared/Top'
 
-import { getAdBanners } from '@/remote/adBanner'
-import { getCards } from '@/remote/card'
-
 const Home = () => {
-  useEffect(() => {
-    getCards().then((response) => console.log('response', response))
-    getAdBanners().then((response) => console.log('response', response))
-  }, [])
   return (
     <div>
       <Top
         title="혜택 좋은 카드"
         subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤어요"
       />
+      <AdBanners />
     </div>
   )
 }
