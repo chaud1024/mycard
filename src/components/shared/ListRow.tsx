78a67e8689283038d5f35d52204d39a009a219a3
chaud1024 <chaud1024@gmail.com>
2024-03-03 14:15:57 +0900

    svg 속성 수정

--- src/components/shared/ListRow.tsx
+++ src/components/shared/ListRow.tsx
@@ -62,9 +62,9 @@ function IconArrowRight() {
       width="24"
       height="24"
       stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
