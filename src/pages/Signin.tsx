ab1c8aab60239bb689b39489a092dde8fe227154
chaud1024 <chaud1024@gmail.com>
2024-03-04 23:25:16 +0900

    로그인 Signin 화면 onSubmit 로직

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,11 +1,44 @@
-import { FormValues } from '@/models/signin'
-import Form from '@components/signin/Form'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import { useCallback } from 'react'
 
+import { FormValues } from '@/models/signin'
+import Form from '@components/signin/Form'
+import { useAlertContext } from '@contexts/AlertContext'
+import { auth } from '@remote/firebase'
+import { FirebaseError } from 'firebase/app'
+import { useNavigate } from 'react-router-dom'
+
 const SigninPage = () => {
-  const handleSubmit = useCallback((formValues: FormValues) => {
-    console.log('로그인 시도하는 정보', formValues)
-  }, [])
+  const { open } = useAlertContext()
+  const navigate = useNavigate()
+
+  const handleSubmit = useCallback(
+    async (formValues: FormValues) => {
+      const { email, password } = formValues
+
+      try {
+        await signInWithEmailAndPassword(auth, email, password)
+        navigate('/')
+      } catch (e) {
+        // firebase 에러
+        if (e instanceof FirebaseError) {
+          if (e.code === 'auth/invalid-credential') {
+            open({
+              title: '계정 정보를 다시 확인해주세요',
+              onButtonClick: () => {},
+            })
+            return
+          }
+        }
+        // 일반 에러
+        open({
+          title: '잠시후 다시 시도해주세요',
+          onButtonClick: () => {},
+        })
+      }
+    },
+    [open],
+  )
   return (
     <div>
       <Form onSubmit={handleSubmit} />
