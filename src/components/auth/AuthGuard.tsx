f026e72a7ec2aecfd80d29d079b7d5f2152b1dc6
chaud1024 <chaud1024@gmail.com>
2024-03-04 23:26:28 +0900

    userAtom으로 사용자 상태 처리

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -1,22 +1,34 @@
 import React, { useState } from 'react'
 
+import { userAtom } from '@atoms/user'
 import { auth } from '@remote/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
+import { useSetRecoilState } from 'recoil'
 
 // 인증처리를 위한 컴포넌트
 // AuthGuard 컴포넌트를 거침 ? 인증 처리 완료
 const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   const [initialize, setInitialize] = useState(false)
+  const setUser = useSetRecoilState(userAtom)
 
   // 파이어베이스의 인증상태가 바뀌면 동작하는 함수
   onAuthStateChanged(auth, (user) => {
-    console.log('user', user)
     // 인증처리완료 ? App 컴포넌트 그려주기
+    if (user != null) {
+      setUser({
+        uid: user.uid,
+        email: user.email ?? '',
+        displayName: user.displayName ?? '',
+      })
+    } else {
+      setUser(null)
+    }
+
     setInitialize(true)
   })
 
   if (initialize === false) {
-    return <div>로딩중...</div>
+    return null
   }
 
   return <>{children}</>
