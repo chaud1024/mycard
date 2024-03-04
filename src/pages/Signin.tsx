ad3f2973a8cb7ca6a06d689e0fe21910e206ce67
chaud1024 <chaud1024@gmail.com>
2024-03-04 22:24:40 +0900

    생성: 로그인 Signin 화면, onSubmit 로직

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { FormValues } from '@/models/signin'
+import Form from '@components/signin/Form'
+import { useCallback } from 'react'
 
 const SigninPage = () => {
-  return <div>SigninPage</div>
+  const handleSubmit = useCallback((formValues: FormValues) => {
+    console.log('로그인 시도하는 정보', formValues)
+  }, [])
+  return (
+    <div>
+      <Form onSubmit={handleSubmit} />
+    </div>
+  )
 }
 
 export default SigninPage
