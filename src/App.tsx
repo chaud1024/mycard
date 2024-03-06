7d099912a5fad0b04fd41e7ca70668e2c67c7a0b
chaud1024 <chaud1024@gmail.com>
2024-03-06 23:17:38 +0900

    수정: 로그인 상태 여부에 따른 분기처리

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import Home from '@pages/Home'
 import SigninPage from '@pages/Signin'
 import SignupPage from '@pages/Signup'
 import Test from '@pages/Test'
+import ApplyPage from '@pages/Apply'
 import ScrollToTop from '@shared/ScrollToTop'
-import NavBar from './components/shared/NavBar'
+import NavBar from '@components/shared/NavBar'
+import PrivateRoute from '@components/auth/PrivateRoute'
 
 const App = () => {
   return (
@@ -18,6 +20,14 @@ const App = () => {
         <Route path="/signin" Component={SigninPage} />
         <Route path="/signup" Component={SignupPage} />
         <Route path="/card/:id" Component={CardPage} />
+        <Route
+          path="/apply/:id"
+          element={
+            <PrivateRoute>
+              <ApplyPage />
+            </PrivateRoute>
+          }
+        />
         <Route path="/test" Component={Test} />
       </Routes>
     </BrowserRouter>
