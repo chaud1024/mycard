ee4b06b13b45bfd32379324d45785e2408769480
chaud1024 <chaud1024@gmail.com>
2024-03-06 23:17:45 +0900

    생성: 카드신청화면

---
+++ src/pages/Apply.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import BasicInfo from '@components/apply/BasicInfo'
+import CardInfo from '@components/apply/CardInfo'
+import Terms from '@components/apply/Terms'
+
+const ApplyPage = () => {
+  const [step, setStep] = useState(0)
+
+  return (
+    <div>
+      {step === 0 ? <Terms /> : null}
+      {step === 1 ? <BasicInfo /> : null}
+      {step === 2 ? <CardInfo /> : null}
+    </div>
+  )
+}
+
+export default ApplyPage
