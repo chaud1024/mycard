1e0ece3026c20ecbcf99258e972b5ca16ba2c78e
chaud1024 <chaud1024@gmail.com>
2024-03-08 23:37:33 +0900

    생성: 약관 Term, 카드신청정보 ApplyValues  model

---
+++ src/models/apply.ts
@@ -0,0 +1,16 @@
+import { User } from './user'
+
+export interface Term {
+  id: string
+  link?: string
+  title: string
+}
+
+// 카드신청한 데이터들 모으기
+// 어느 유저가 어느 약관을 동의했으며 언제 어떤 카드를 신청했는지
+export interface ApplyValues {
+  userId: User['uid']
+  terms: Array<Term['id']>
+  appliedAt: Date
+  cardId: string
+}
