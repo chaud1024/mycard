477678c40d4cf3199b5b468527919e93b8ce3f2c
chaud1024 <chaud1024@gmail.com>
2024-03-08 23:38:52 +0900

    추가: Term 컴포넌트 로직 checked, handleAllAgreement, onNext

--- src/components/apply/Terms.tsx
+++ src/components/apply/Terms.tsx
@@ -1,35 +1,71 @@
 import Agreement from '@shared/Agreement'
+import { MouseEvent, useCallback, useState } from 'react'
 
-const Terms = () => {
+import { 약관목록 } from '@constants/apply'
+import FixedBottomButton from '../shared/FixedBottomButton'
+
+const Terms = ({ onNext }: { onNext: (terms: string[]) => void }) => {
+  const [termsAgreement, setTermsAgreement] = useState(() => {
+    return 약관목록.reduce<Record<string, boolean>>(
+      (prev, term) => ({
+        ...prev,
+        [term.id]: false,
+      }),
+      {},
+    )
+  })
+
+  const 모든약관이_동의되었는가 = Object.values(termsAgreement).every(
+    (동의여부) => 동의여부,
+  )
+
+  const handleAllAgreement = useCallback(
+    (_: MouseEvent<HTMLElement>, checked: boolean) => {
+      setTermsAgreement((prevTerms) => {
+        return Object.keys(prevTerms).reduce(
+          (prev, key) => ({
+            ...prev,
+            [key]: checked,
+          }),
+          {},
+        )
+      })
+    },
+    [],
+  )
+
   return (
     <div>
       <Agreement>
         <Agreement.Title
-          checked={true}
-          onChange={(e, checked) => {
-            console.log(e, checked)
-          }}
+          checked={모든약관이_동의되었는가}
+          onChange={handleAllAgreement}
         >
           약관에 모두 동의
         </Agreement.Title>
-        <Agreement.Description
-          checked={true}
-          onChange={(e, checked) => {
-            console.log(e, checked)
-          }}
-        >
-          약관 1
-        </Agreement.Description>
-        <Agreement.Description
-          checked={false}
-          onChange={(e, checked) => {
-            console.log(e, checked)
-          }}
-          link="https://www.naver.com"
-        >
-          약관 2
-        </Agreement.Description>
+
+        {약관목록.map(({ id, title, link }) => (
+          <Agreement.Description
+            checked={termsAgreement[id]}
+            onChange={(_, checked) => {
+              setTermsAgreement((prevTerms) => ({
+                ...prevTerms,
+                [id]: checked,
+              }))
+            }}
+            key={id}
+            link={link}
+          >
+            {title}
+          </Agreement.Description>
+        ))}
       </Agreement>
+
+      <FixedBottomButton
+        label="약관동의"
+        disabled={모든약관이_동의되었는가 === false}
+        onClick={() => onNext(Object.keys(termsAgreement))}
+      />
     </div>
   )
 }
