a8af962c34bd8a21f1494b683a73c2595145216d
chaud1024 <chaud1024@gmail.com>
2024-03-09 23:18:28 +0900

    수정: 자식컴포넌트 BasicInfo로 부터 데이터 받아오기 handleBasicInfoCahnge

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -1,3 +1,4 @@
+import { ApplyValues } from '@models/apply'
 import BasicInfo from '@components/apply/BasicInfo'
 import CardInfo from '@components/apply/CardInfo'
 import Terms from '@components/apply/Terms'
@@ -6,13 +7,18 @@ import { useState } from 'react'
 const ApplyPage = () => {
   const [step, setStep] = useState(1)
 
-  const handleTermsChange = (terms: string[]) => {
+  const handleTermsChange = (terms: ApplyValues['terms']) => {
     console.log('terms', terms)
+  }
+  const handleBasicInfoChange = (
+    infoValues: Pick<ApplyValues, 'salary' | 'creditScore' | 'payDate'>,
+  ) => {
+    console.log('terms', infoValues)
   }
   return (
     <div>
       {step === 0 ? <Terms onNext={handleTermsChange} /> : null}
-      {step === 1 ? <BasicInfo /> : null}
+      {step === 1 ? <BasicInfo onNext={handleBasicInfoChange} /> : null}
       {step === 2 ? <CardInfo /> : null}
     </div>
   )
