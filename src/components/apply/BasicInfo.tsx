84877ec903e30f2685281e4faf1a35fe580485c3
chaud1024 <chaud1024@gmail.com>
2024-03-09 22:57:18 +0900

    수정: 연소득, 신용점수, 결제일 Select UI

--- src/components/apply/BasicInfo.tsx
+++ src/components/apply/BasicInfo.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import Select from '@shared/Select'
+
+import { 결제일옵션, 신용점수옵션, 연소득옵션 } from '@constants/apply'
 
 const BasicInfo = () => {
-  return <div>BasicInfo</div>
+  return (
+    <div>
+      <Select
+        label="연소득"
+        options={연소득옵션}
+        placeholder={연소득옵션[0].label}
+        value=""
+      />
+      <Select
+        label="신용점수"
+        options={신용점수옵션}
+        placeholder="테스트용1"
+        value=""
+      />
+      <Select
+        label="결제일"
+        options={결제일옵션}
+        placeholder="테스트용2"
+        value=""
+      />
+    </div>
+  )
 }
 
 export default BasicInfo
