41d8b0a1c2d22caa1472e748a018dd59a9fc7fb0
chaud1024 <chaud1024@gmail.com>
2024-03-10 16:55:27 +0900

    수정: 카드신청 단계별 setApplyValues 데이터 전달

--- src/components/apply/index.tsx
+++ src/components/apply/index.tsx
@@ -1,22 +1,63 @@
 import BasicInfo from '@components/apply/BasicInfo'
 import CardInfo from '@components/apply/CardInfo'
 import Terms from '@components/apply/Terms'
-import { ApplyValues } from '@models/apply'
+import useUser from '@hooks/auth/useUser'
+import { ApplyValues, APPLY_STATUS } from '@models/apply'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
-const Apply = ({ step, onSubmit }: { step: number; onSubmit: () => void }) => {
+const Apply = ({
+  onSubmit,
+}: {
+  onSubmit: (applyValues: ApplyValues) => void
+}) => {
+  const user = useUser()
+  const { id } = useParams() as { id: string }
+
+  const [step, setStep] = useState(0)
+  const [applyValues, setApplyValues] = useState<Partial<ApplyValues>>({
+    userId: user?.uid,
+    cardId: id,
+  })
+
+  useEffect(() => {
+    if (step === 3) {
+      onSubmit({
+        ...applyValues,
+        appliedAt: new Date(),
+        status: APPLY_STATUS.READY,
+      } as ApplyValues)
+    }
+  }, [step, applyValues, onSubmit])
+
   const handleTermsChange = (terms: ApplyValues['terms']) => {
-    console.log('terms', terms)
+    setApplyValues((prev) => ({
+      ...prev,
+      terms,
+    }))
+
+    setStep((prevStep) => prevStep + 1)
   }
   const handleBasicInfoChange = (
     infoValues: Pick<ApplyValues, 'salary' | 'creditScore' | 'payDate'>,
   ) => {
-    console.log('infoValues', infoValues)
+    setApplyValues((prev) => ({
+      ...prev,
+      ...infoValues,
+    }))
+
+    setStep((prevStep) => prevStep + 1)
   }
 
   const handleCardInfoChange = (
     cardInfoValues: Pick<ApplyValues, 'isMaster' | 'isHipass' | 'isRf'>,
   ) => {
-    console.log('cardInfoValues', cardInfoValues)
+    setApplyValues((prev) => ({
+      ...prev,
+      ...cardInfoValues,
+    }))
+
+    setStep((prevStep) => prevStep + 1)
   }
   return (
     <div>
