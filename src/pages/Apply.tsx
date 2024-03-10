9b747ac441bcb5b8d9e70bb1b4493baeef62b317
chaud1024 <chaud1024@gmail.com>
2024-03-10 17:55:51 +0900

    추가: 중도이탈 및 기신청 카드 정보에 따른 로직 추가

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
+import FullPageLoader from '@/components/shared/FullPageLoader'
+import { useAlertContext } from '@/contexts/AlertContext'
 import Apply from '@components/apply'
+import useAppliedCard from '@components/apply/hooks/useAppliedCard'
 import useApplyCardMutation from '@components/apply/hooks/useApplyCardMutation'
 import usePollApplyStatus from '@components/apply/hooks/usePollApplyStatus'
 import useUser from '@hooks/auth/useUser'
@@ -10,11 +13,41 @@ import { updateApplyCard } from '@remote/apply'
 
 const ApplyPage = () => {
   const navigate = useNavigate()
+  const { open } = useAlertContext()
 
   const [readyToPoll, setReadyToPoll] = useState(false)
 
   const user = useUser()
   const { id } = useParams() as { id: string }
+
+  // 사용자가 현재의 카드를 신청했는지 데이터 확인
+  const { data } = useAppliedCard({
+    userId: user?.uid as string,
+    cardId: id,
+    options: {
+      onSuccess: (applied) => {
+        if (applied == null) {
+          return
+        }
+
+        if (applied.status === APPLY_STATUS.COMPLETE) {
+          open({
+            title: '이미 발급이 완료된 카드입니다.',
+            onButtonClick: () => {
+              window.history.back()
+            },
+          })
+
+          return
+        }
+
+        // 신청정보 있고, 완료는 되지 않은 상태일 때 => 폴링시작(재심사 케이스)
+        setReadyToPoll(true)
+      },
+      onError: () => {},
+      suspense: true,
+    },
+  })
 
   usePollApplyStatus({
     onSuccess: async () => {
@@ -54,8 +87,13 @@ const ApplyPage = () => {
     },
   })
 
+  // 신청한 카드정보 데이터가 없고 && 데이터의 신청 상태가 완료라면
+  if (data != null && data.status === APPLY_STATUS.COMPLETE) {
+    return null
+  }
+
   if (readyToPoll || 카드를신청중인가) {
-    return <div>Loading...</div>
+    return <FullPageLoader message="카드를 신청중입니다" />
   }
   return <Apply onSubmit={mutate} />
 }
