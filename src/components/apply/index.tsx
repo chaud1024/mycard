a21ad196920cc5a8f6d5c865e6e84e2548131ed2
chaud1024 <chaud1024@gmail.com>
2024-03-10 17:54:48 +0900

    수정: step 을 useState에서 ApplyValues의 타입으로 변경함에 따른 코드 수정 및 localStorage에 단계별 데이터 임시저장

--- src/components/apply/index.tsx
+++ src/components/apply/index.tsx
@@ -14,29 +14,43 @@ const Apply = ({
   const user = useUser()
   const { id } = useParams() as { id: string }
 
-  const [step, setStep] = useState(0)
-  const [applyValues, setApplyValues] = useState<Partial<ApplyValues>>({
-    userId: user?.uid,
-    cardId: id,
+  const storageKey = `applied-${user?.uid}-${id}`
+
+  const [applyValues, setApplyValues] = useState<Partial<ApplyValues>>(() => {
+    const applied = localStorage.getItem(storageKey)
+
+    // 진행상태가 초기값이라면
+    if (applied == null) {
+      return {
+        userId: user?.uid,
+        cardId: id,
+        step: 0,
+      }
+    }
+
+    return JSON.parse(applied)
   })
 
   useEffect(() => {
-    if (step === 3) {
+    if (applyValues.step === 3) {
+      // 마지막 단계가 되어 실제 데이터를 서버로 전송할 때 localStorage에 있던 임시값을 삭제
+      localStorage.removeItem(storageKey)
       onSubmit({
         ...applyValues,
         appliedAt: new Date(),
         status: APPLY_STATUS.READY,
       } as ApplyValues)
+    } else {
+      localStorage.setItem(storageKey, JSON.stringify(applyValues))
     }
-  }, [step, applyValues, onSubmit])
+  }, [applyValues, onSubmit, storageKey])
 
   const handleTermsChange = (terms: ApplyValues['terms']) => {
     setApplyValues((prev) => ({
       ...prev,
       terms,
+      step: (prev.step as number) + 1,
     }))
-
-    setStep((prevStep) => prevStep + 1)
   }
   const handleBasicInfoChange = (
     infoValues: Pick<ApplyValues, 'salary' | 'creditScore' | 'payDate'>,
@@ -44,9 +58,8 @@ const Apply = ({
     setApplyValues((prev) => ({
       ...prev,
       ...infoValues,
+      step: (prev.step as number) + 1,
     }))
-
-    setStep((prevStep) => prevStep + 1)
   }
 
   const handleCardInfoChange = (
@@ -55,15 +68,18 @@ const Apply = ({
     setApplyValues((prev) => ({
       ...prev,
       ...cardInfoValues,
+      step: (prev.step as number) + 1,
     }))
-
-    setStep((prevStep) => prevStep + 1)
   }
   return (
     <div>
-      {step === 0 ? <Terms onNext={handleTermsChange} /> : null}
-      {step === 1 ? <BasicInfo onNext={handleBasicInfoChange} /> : null}
-      {step === 2 ? <CardInfo onNext={handleCardInfoChange} /> : null}
+      {applyValues.step === 0 ? <Terms onNext={handleTermsChange} /> : null}
+      {applyValues.step === 1 ? (
+        <BasicInfo onNext={handleBasicInfoChange} />
+      ) : null}
+      {applyValues.step === 2 ? (
+        <CardInfo onNext={handleCardInfoChange} />
+      ) : null}
     </div>
   )
 }
